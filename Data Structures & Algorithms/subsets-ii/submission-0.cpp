9762ff2894ba7a3e849d66dcdbe71803f1afaace
Abhinav <71382020+Abhinav0358@users.noreply.github.com>
2026-05-20 19:53:23 +0530

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        // first ig lets sort
+        // then avoid repetitions
+        // repetition - include first 1 then exclude next 1 , exclude first 1 then include next 1
+        vector<vector<int>> ans;
+        vector<int> currvec;
+        sort(nums.begin(),nums.end());
+        backtracking(ans, nums,0, currvec);
+        return ans;
+
+    }void backtracking(vector<vector<int>> &ans, vector<int> nums, int index, vector<int> currvec){
+        for(int i=index; i<=nums.size(); i++){
+            if(i>=nums.size()){
+                ans.push_back(currvec);
+                return;
+            }
+            if(i>index&&nums[i]==nums[i-1])continue;
+            currvec.push_back(nums[i]);
+            backtracking(ans,nums,i+1, currvec);
+            currvec.pop_back();
+        }
+    }
+};
