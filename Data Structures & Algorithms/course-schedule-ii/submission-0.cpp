b19e265478868849d7dbc1d60f08f7c0cc3333f6
Abhinav <71382020+Abhinav0358@users.noreply.github.com>
2026-05-27 20:56:18 +0530

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+    vector<vector<int>> adj(numCourses);
+    vector<int>prereqcount(numCourses,0);
+    vector<int> ans;
+    vector<int> empty;
+    // making the adjoint
+
+    for(vector<int> &pear : prerequisites){
+        adj[pear[1]].push_back(pear[0]);
+        prereqcount[pear[0]]++;
+    }
+
+    queue<int> que;
+    int count=0;
+    for(int i=0; i<numCourses; i++){
+        if(prereqcount[i]==0)que.push(i);
+    }
+
+    while(!que.empty()){
+        int p = que.front();
+        que.pop();
+        ans.push_back(p);
+        count++;
+        for(int j=0; j<adj[p].size(); j++){
+            prereqcount[adj[p][j]]--;
+            if(prereqcount[adj[p][j]]==0)que.push(adj[p][j]);
+        }
+    }
+    return (numCourses==count? ans:empty);
+    }
+};
