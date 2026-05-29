9681f5cf88435e6f1ff364f9557132b93099f550
Abhinav <71382020+Abhinav0358@users.noreply.github.com>
2026-05-29 19:35:17 +0530

    Add: redundant-connection - submission-1

---
+++ Data Structures & Algorithms/redundant-connection/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        vector<int> parents(edges.size()+1);
+        for(int i=0; i<parents.size();i++){
+            parents[i]=i;
+            // cout<<parents[i]<<" ";
+        }
+        cout<<endl;
+        vector<int>ans;
+        for(auto & edge: edges){
+            if(!unioon(edge[0],edge[1],parents))ans=edge;
+            // for(int i=0; i<parents.size();i++){
+            //     cout<<find(i,parents)<<" ";
+            // }
+            // cout<<endl;
+        }
+        return ans;
+    }
+    int find(int a, vector<int> & parents ){
+        if(parents[a]==a)return a;
+        return find(parents[a],parents);
+    }
+    bool unioon(int a, int b, vector<int> & parents){
+        if(find(a,parents)==find(b,parents))return false;
+        parents[find(a,parents)]=find(b,parents);
+        return true;
+    }
+};
