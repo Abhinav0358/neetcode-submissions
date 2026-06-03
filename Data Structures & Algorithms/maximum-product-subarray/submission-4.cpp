b3433676d9ab6f206b5decb3e048d8f85b1940d9
Abhinav <71382020+Abhinav0358@users.noreply.github.com>
2026-06-03 10:00:06 +0530

    Add: maximum-product-subarray - submission-4

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-4.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        if(nums.size()==0)return 0;
+        int maxprod=nums[0];
+        int minprod=nums[0];
+        int globalmax=nums[0];
+        for(int i=1; i<nums.size(); i++){
+            int tmp=max({nums[i],nums[i]*maxprod,nums[i]*minprod});
+            
+            
+            minprod=min({nums[i],nums[i]*maxprod,nums[i]*minprod});
+            
+            maxprod=tmp;
+            
+            globalmax=max(maxprod,globalmax);
+            cout<<globalmax<<endl;
+        }
+        return globalmax;
+    }
+};
