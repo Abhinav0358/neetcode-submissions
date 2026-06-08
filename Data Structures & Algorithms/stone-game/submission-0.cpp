d21041878627cdd23664251913fb7f605e454cb6
Abhinav <71382020+Abhinav0358@users.noreply.github.com>
2026-06-08 20:56:04 +0530

    Add: stone-game - submission-0

---
+++ Data Structures & Algorithms/stone-game/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        // int firstindex=0;
+        // int lastindex=piles.size()-1;
+
+        // int alicecount=0;
+        // int bobcount=0;
+
+        // while(lastindex>firstindex){
+        //     if(piles[firstindex]>piles[lastindex]){
+        //         alicecount+=piles[firstindex];
+        //         firstindex++;
+        //     }else if(piles[firstindex]<piles[lastindex]){
+        //         alicecount+=piles[lastindex];
+        //         lastindex--;
+        //     }
+        //     if(piles[firstindex]>piles[lastindex]){
+        //         bobcount+=piles[firstindex];
+        //         firstindex++;
+        //     }else if(piles[firstindex]<piles[lastindex]){
+        //         bobcount+=piles[lastindex];
+        //         lastindex--;
+        //     }
+        // }
+        return true;
+        // // return alicecount>bobcount;
+
+    }
+};
