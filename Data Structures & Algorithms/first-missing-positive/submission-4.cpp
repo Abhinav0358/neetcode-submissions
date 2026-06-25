48ca8cb0f136cc177c20463244b553eb8b4d3d31
Abhinav <71382020+Abhinav0358@users.noreply.github.com>
2026-06-25 16:16:54 +0530

    Add: first-missing-positive - submission-4

---
+++ Data Structures & Algorithms/first-missing-positive/submission-4.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        // for(int i=0; i<nums.size(); i++){
+        //     cout<<nums[i]<<" ";
+        // }
+        for(int i=0; i<nums.size(); i++){
+            puttocorrectplace(nums,nums[i]);
+        }
+        // cout<<endl;
+        // for(int i=0; i<nums.size(); i++){
+        //     cout<<nums[i]<<" ";
+        // }
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]!=i+1)return i+1;
+        }
+        return nums.size()+1;
+    }
+    void puttocorrectplace(vector<int>& nums, int i){
+        if(i<1||i>nums.size())return;
+        if(nums[i-1]==i)return;
+        int tmp=nums[i-1];
+        nums[i-1]=i;
+        puttocorrectplace(nums,tmp);
+    }
+};  
