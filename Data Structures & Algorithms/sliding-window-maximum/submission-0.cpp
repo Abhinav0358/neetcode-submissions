1ad7600afb05e419883769d45812deecca468823
Abhinav <71382020+Abhinav0358@users.noreply.github.com>
2026-06-29 10:18:35 +0530

    Add: sliding-window-maximum - submission-0

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-0.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int max=nums[0];
+        int maxindex=0;
+        vector<int> ans;
+        for(int i=0; i<k; i++){
+            if(nums[i]>=max){
+                max=nums[i];
+                maxindex=i;
+            }
+        }
+        ans.push_back(max);
+        int start=0;
+        int end=k-1;
+        while(end<nums.size()){
+            start++;
+            end++;
+            if(end>=nums.size())break;
+            if(nums[end]>=max&&maxindex>=start){
+                max=nums[end];
+                maxindex=end;
+            }
+            if(maxindex<start){
+                maxindex=findmax(nums,start,end);
+                max=nums[maxindex];
+            }
+            ans.push_back(max);
+        }
+        return ans;
+    }
+    int findmax(vector<int> &nums, int start, int end){
+        int max=nums[start];
+        int maxindex=start;
+        for(int i=start; i<=end; i++){
+            if(nums[i]>=max){
+                max=nums[i];
+                maxindex=i;
+            }
+        }
+        return maxindex;
+    }
+};
